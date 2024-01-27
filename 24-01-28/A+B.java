64c09f5d9f89e0bca081ae8a8e932c129e3ffeff
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-01-28 06:19:55 +0900

    auth_commited

---
+++ 24-01-28/A+B.java
@@ -0,0 +1,13 @@
+// 문제 : A+B
+// 결과 :  / 속도: 220 / 메모리 : 17688
+// 제출시각 : 24-01-28  6:19:53
+import java.util.Scanner;
+​
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
